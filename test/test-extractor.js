c39fccad13915b03d7dbba59071407aea414eebb
jerome creignou <jerome.creignou@gmail.com>
2012-07-24 03:12:22 +0200

    add proper testing

--- test/test-extractor.js
+++ test/test-extractor.js
@@ -1,25 +1,52 @@
 var vows = require('vows'),
     assert = require('assert'),
-    extractor = require('../lib/main.js');
+    extractor = require('../lib/main.js'),
+    fs = require('fs');
     
 vows.describe('Extractor').addBatch({
-    'Extractor object : ':{
+    'when creating an Extractor object,':{
         topic: extractor(),
-        'as matches method :' : function(e){
-            assert.ok(e.matches);
-        },
-        'as start method :' : function(e){
-            assert.ok(e.start);
+        'we get an object which':{
+            'has a "matches" method' : function(e){
+                assert.ok(e.matches);
+            },
+            'has a "start" method' : function(e){
+                assert.ok(e.start);
+            },
+            'has a "watch" method' : function(e){
+                assert.ok(e.watch);
+            }
         }
-    }
-}).addBatch({
-    'Extractor object with accumulator : ':{
-        topic: extractor({myvar:0}),
-        'as matches method :' : function(e){
-            assert.ok(e.matches);
+    },
+    'when creating an Extractor object with an accumulator,':{
+        topic: extractor({count:0}),
+        'we get an object which':{
+            'has a "matches" method' : function(e){
+                assert.ok(e.matches);
+            },
+            'has a "start" method' : function(e){
+                assert.ok(e.start);
+            },
+            'has a "watch" method' : function(e){
+                assert.ok(e.watch);
+            }
         },
-        'as start method :' : function(e){
-            assert.ok(e.start);
+        'we get notified':{
+            topic: function(ext){
+                var s = fs.createReadStream(__dirname + '/sample.csv',{});
+                ext.matches(/;(?!(?:[^",]|[^"],[^"])+")/,function(m,vars){
+                    vars.count ++;
+                });
+                //ext.on('end', this.callback);
+                return ext.start(s);
+            },
+            on:{ 
+                "end" : {
+                    "we have a 25 lines" : function(e){
+                        assert.equal(e.count,25);
+                    }
+                }
+            }
         }
     }
 }).export(module);
